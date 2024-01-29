ff838c5bb499461c92b0ae576cd6219963c84767
parkjiwon <ji11won@naver.com>
2024-01-29 11:40:54 +0900

    Feat : Swipe action 추가 및 layout 변경
    - SwipeHelperCallback : swipe action 정의

--- app/src/main/java/konkuk/gdsc/memotion/ui/diary/DiaryFragment.kt
+++ app/src/main/java/konkuk/gdsc/memotion/ui/diary/DiaryFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import konkuk.gdsc.memotion.data.DiarySimple
 import konkuk.gdsc.memotion.databinding.FragmentDiaryBinding
+import konkuk.gdsc.memotion.util.dpToPx
 
 class DiaryFragment : Fragment() {
 
@@ -24,8 +28,26 @@ class DiaryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.apply {
+        val diaryAdapter = DiaryAdapter(requireActivity(), DiarySimple.sample)
 
+        binding.apply {
+            rvDiaryList.adapter = diaryAdapter
+            rvDiaryList.layoutManager = LinearLayoutManager(requireActivity())
+        }
+
+        // 리사이클러뷰에 스와이프, 드래그 기능 달기
+        val swipeHelperCallback = SwipeHelperCallback(diaryAdapter).apply {
+            // 스와이프한 뒤 고정시킬 위치 지정
+            val size = dpToPx(requireActivity(), 51f)
+            setClamp((resources.displayMetrics.widthPixels.toFloat() - size) / 4)    // 1080 / 4 = 270
+        }
+
+        ItemTouchHelper(swipeHelperCallback).attachToRecyclerView(binding.rvDiaryList)
+
+        // 다른 곳 터치 시 기존 선택했던 뷰 닫기
+        binding.rvDiaryList.setOnTouchListener { _, _ ->
+            swipeHelperCallback.removePreviousClamp(binding.rvDiaryList)
+            false
         }
     }
 
