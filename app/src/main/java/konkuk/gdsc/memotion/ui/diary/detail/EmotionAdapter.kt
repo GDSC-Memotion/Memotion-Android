daec1ddc01d65017c2103243ceb0d6fd402c18f8
parkjiwon <ji11won@naver.com>
2024-01-29 11:38:27 +0900

    Feat : DiaryDetailActivity 추가
    - AndroidManifest : activity 추가
    - EmotionAdapter : 결과창의 감정 recyclerview의 adapter

---
+++ app/src/main/java/konkuk/gdsc/memotion/ui/diary/detail/EmotionAdapter.kt
@@ -0,0 +1,35 @@
+package konkuk.gdsc.memotion.ui.diary.detail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import konkuk.gdsc.memotion.data.EmotionResult
+import konkuk.gdsc.memotion.databinding.ItemEmotionPercentageBinding
+
+class EmotionAdapter(
+    private val data: List<EmotionResult>
+) : RecyclerView.Adapter<EmotionAdapter.ViewHolder>() {
+
+    class ViewHolder(
+        val binding: ItemEmotionPercentageBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: EmotionResult) {
+            binding.apply {
+                tvItemEmotionTitle.text = item.emotion.toString()
+                tvItemEmotionPercentageNumber.text = String.format("%.2f", item.percentage) + "%"
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemEmotionPercentageBinding =
+            ItemEmotionPercentageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    override fun getItemCount() = data.size
+}
