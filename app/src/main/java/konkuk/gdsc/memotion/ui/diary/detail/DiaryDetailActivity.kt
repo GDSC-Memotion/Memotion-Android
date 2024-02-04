261fff3a464e102963332586d8897349f8a02b27
parkjiwon <ji11won@naver.com>
2024-02-04 18:38:00 +0900

    Feat : 기존 팝업 삭제 후 balloon으로 변경
    - 기존 팝업 : menu_popup
    - popup_diary_detail : balloon layout
    - DiaryDetailActivity : 기능 변경

--- app/src/main/java/konkuk/gdsc/memotion/ui/diary/detail/DiaryDetailActivity.kt
+++ app/src/main/java/konkuk/gdsc/memotion/ui/diary/detail/DiaryDetailActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.skydoves.balloon.ArrowOrientation
+import com.skydoves.balloon.Balloon
+import com.skydoves.balloon.BalloonAnimation
+import com.skydoves.balloon.createBalloon
 import konkuk.gdsc.memotion.R
 import konkuk.gdsc.memotion.data.DiaryDetail
 import konkuk.gdsc.memotion.databinding.ActivityDiaryDetailBinding
@@ -14,7 +20,24 @@ import konkuk.gdsc.memotion.ui.diary.create.WritingDiaryActivity
 class DiaryDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDiaryDetailBinding
-    val data = DiaryDetail.sample
+    private val data = DiaryDetail.sample
+    private val balloon = createBalloon(this@DiaryDetailActivity) {
+        setLayout(R.layout.popup_diary_detail)
+        setArrowSize(0)
+        setArrowOrientation(ArrowOrientation.TOP)
+        setCornerRadius(4f)
+        setMarginRight(20)
+        setMarginTop(8)
+        setBackgroundColor(
+            ContextCompat.getColor(
+                this@DiaryDetailActivity,
+                R.color.white
+            )
+        )
+        setBalloonAnimation(BalloonAnimation.ELASTIC)
+        setLifecycleOwner(this@DiaryDetailActivity)
+        build()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,25 +52,7 @@ class DiaryDetailActivity : AppCompatActivity() {
             }
 
             ivDiaryDetailMenu.setOnClickListener {
-                val popup = PopupMenu(this@DiaryDetailActivity, ivDiaryDetailMenu)
-                popup.inflate(R.menu.menu_popup)
-                popup.setOnMenuItemClickListener { item ->
-                    when (item.itemId) {
-                        R.id.popup_item_edit -> {
-                            val intent =
-                                Intent(this@DiaryDetailActivity, WritingDiaryActivity::class.java)
-                            startActivity(intent)
-                        }
-
-                        R.id.popup_item_delete -> {
-
-                        }
-
-                        else -> false
-                    }
-                    true
-                }
-                popup.show()
+                balloon.showAlignBottom(binding.ivDiaryDetailMenu)
             }
 
             tvDiaryDetailDate.text = data.date
@@ -61,7 +66,10 @@ class DiaryDetailActivity : AppCompatActivity() {
             )
 
             tvDiaryDetailEmotion.text = titleEmotion?.emotion.toString()
-            lpiDiaryDetailPercentage.setProgressCompat((data.emotions[0].percentage * 100).toInt(), true)
+            lpiDiaryDetailPercentage.setProgressCompat(
+                (data.emotions[0].percentage * 100).toInt(),
+                true
+            )
             tvDiaryDetailPercentageNumber.text = titleEmotion?.percentage.toString()
 
             rvDiaryDetailEmotionList.adapter = EmotionAdapter(data.emotions)
@@ -74,9 +82,31 @@ class DiaryDetailActivity : AppCompatActivity() {
             }
             cvDiaryDetailYoutubeMusic.setOnClickListener {
                 val implicitIntent =
-                    Intent(Intent.ACTION_VIEW, Uri.parse("https://www.music." + data.youtubeUrl))
+                    Intent(Intent.ACTION_VIEW, Uri.parse("https://music." + data.youtubeUrl))
                 startActivity(implicitIntent)
             }
+        }
+
+        setPopupButton()
+    }
+
+    private fun setPopupButton() {
+        val editButton: LinearLayoutCompat =
+            balloon.getContentView().findViewById(R.id.ll_popup_edit)
+        val trashButton: LinearLayoutCompat =
+            balloon.getContentView().findViewById(R.id.ll_popup_trash)
+
+
+        editButton.setOnClickListener {
+            val intent =
+                Intent(this@DiaryDetailActivity, WritingDiaryActivity::class.java)
+            startActivity(intent)
+            balloon.dismiss()
+        }
+
+        trashButton.setOnClickListener {
+            balloon.dismiss()
+            finish()
         }
     }
 }
