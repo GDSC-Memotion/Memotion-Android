399c3372e473944263ccc8ca595246b736482627
parkjiwon <ji11won@naver.com>
2024-02-13 03:59:14 +0900

    Feat : FragmentEmotionSelect RadioGroup

--- app/src/main/java/konkuk/gdsc/memotion/ui/diary/dialog/FragmentEmotionSelect.kt
+++ app/src/main/java/konkuk/gdsc/memotion/ui/diary/dialog/FragmentEmotionSelect.kt
@@ -6,7 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.annotation.IdRes
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import konkuk.gdsc.memotion.R
 import konkuk.gdsc.memotion.databinding.FragmentEmotionSelectBinding
@@ -21,9 +22,7 @@ class FragmentEmotionSelect(
     private val binding: FragmentEmotionSelectBinding
         get() = requireNotNull(_binding) { "FragmentEmotionSelect's binding is null" }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val emotinoResult: MutableLiveData<Emotion> = MutableLiveData()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,45 +40,60 @@ class FragmentEmotionSelect(
             llEmotionSelectBackground.setOnClickListener {}
 
             btnEmotionSelect.setOnClickListener {
-                btnSelect(Emotion.ANGER)
+                emotinoResult.value?.let { btnSelect(it) }
             }
+
+            rbEmotion1.setOnClickListener {
+                rgEmotionSelect2.clearCheck()
+                emotinoResult.value = getEmotionResult(it.id)
+            }
+            rbEmotion2.setOnClickListener {
+                rgEmotionSelect2.clearCheck()
+                emotinoResult.value = getEmotionResult(it.id)
+            }
+            rbEmotion3.setOnClickListener {
+                rgEmotionSelect2.clearCheck()
+                emotinoResult.value = getEmotionResult(it.id)
+            }
+            rbEmotion4.setOnClickListener {
+                emotinoResult.value = getEmotionResult(it.id)
+                rgEmotionSelect2.clearCheck()
+            }
+            rbEmotion5.setOnClickListener {
+                emotinoResult.value = getEmotionResult(it.id)
+                rgEmotionSelect1.clearCheck()
+            }
+            rbEmotion6.setOnClickListener {
+                emotinoResult.value = getEmotionResult(it.id)
+                rgEmotionSelect1.clearCheck()
+            }
+            rbEmotion7.setOnClickListener {
+                emotinoResult.value = getEmotionResult(it.id)
+                rgEmotionSelect1.clearCheck()
+            }
+
         }
 
-        /*binding.rgEmotionSelect1.setOnCheckedChangeListener { radioGroup, i ->
-            binding.rgEmotionSelect2.clearCheck()
-            binding.rgEmotionSelect3.clearCheck()
+        val observer = Observer<Emotion> {
+            binding.btnEmotionSelect.isEnabled = true
         }
-        binding.rgEmotionSelect2.setOnCheckedChangeListener { radioGroup, i ->
-            binding.rgEmotionSelect1.clearCheck()
-            binding.rgEmotionSelect3.clearCheck()
-        }
-        binding.rgEmotionSelect3.setOnCheckedChangeListener { radioGroup, i ->
-            binding.rgEmotionSelect1.clearCheck()
-            binding.rgEmotionSelect2.clearCheck()
-        }
-
-        val observer = Observer<Int>{ id ->
-            Log.d(TAG, "FragmentEmotionSelect - observer 활성화\nid:$id")
-            when(id){
-                R.id.rb_emotion_1, R.id.rb_emotion_2, R.id.rb_emotion_3 -> {
-                    binding.rgEmotionSelect2.clearCheck()
-                    binding.rgEmotionSelect3.clearCheck()
-                }
-                R.id.rb_emotion_4, R.id.rb_emotion_5, R.id.rb_emotion_6 -> {
-                    binding.rgEmotionSelect1.clearCheck()
-                    binding.rgEmotionSelect3.clearCheck()
-                }
-                R.id.rb_emotion_6-> {
-                    binding.rgEmotionSelect1.clearCheck()
-                    binding.rgEmotionSelect2.clearCheck()
-                }
-            }
-        }
-        viewModel.currentId.observe(viewLifecycleOwner, observer)*/
+        emotinoResult.observe(viewLifecycleOwner, observer)
     }
 
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun getEmotionResult(@IdRes id: Int): Emotion {
+        return when (id) {
+            R.id.rb_emotion_2 -> Emotion.DISGUST
+            R.id.rb_emotion_3 -> Emotion.FEAR
+            R.id.rb_emotion_4 -> Emotion.JOY
+            R.id.rb_emotion_5 -> Emotion.NEUTRAL
+            R.id.rb_emotion_6 -> Emotion.SADNESS
+            R.id.rb_emotion_7 -> Emotion.SURPRISE
+            else -> Emotion.ANGER
+        }
     }
 }
