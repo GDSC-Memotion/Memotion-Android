e9016d29ae0c6718f53b0aa7ceeeb35fcfd14a4f
parkjiwon <ji11won@naver.com>
2024-02-13 03:58:04 +0900

    Feat : 오타 수정 및 리소스 연결

--- app/src/main/java/konkuk/gdsc/memotion/domain/entity/emotion/Emotion.kt
+++ app/src/main/java/konkuk/gdsc/memotion/domain/entity/emotion/Emotion.kt
@@ -5,21 +5,21 @@ import konkuk.gdsc.memotion.R
 enum class Emotion(val number: Int) {
     ANGER(0),
     DISGUST(1),
-    FEAT(2),
+    FEAR(2),
     JOY(3),
     NEUTRAL(4),
     SADNESS(5),
     SURPRISE(6);
 
     fun getResource(): Int {
-        return when(this) {
-            ANGER -> R.drawable.icon_anger
-            DISGUST -> TODO()
-            FEAT -> TODO()
-            JOY -> TODO()
-            NEUTRAL -> TODO()
-            SADNESS -> TODO()
-            SURPRISE -> TODO()
+        return when (this) {
+            ANGER -> R.drawable.icon_emoji_anger
+            DISGUST -> R.drawable.icon_emoji_disgust
+            FEAR -> R.drawable.icon_emoji_fear
+            JOY -> R.drawable.icon_emoji_joy
+            NEUTRAL -> R.drawable.icon_emoji_neutral
+            SADNESS -> R.drawable.icon_emoji_sadness
+            SURPRISE -> R.drawable.icon_emoji_surprise
         }
     }
 
--- app/src/main/java/konkuk/gdsc/memotion/domain/entity/emotion/EmotionDTO.kt
+++ app/src/main/java/konkuk/gdsc/memotion/domain/entity/emotion/EmotionDTO.kt
@@ -4,16 +4,16 @@ data class EmotionResult(
     val emotion: Emotion,
     val percentage: Float,
     val isTitle: Boolean,
-){
+) {
     companion object {
         val sample: List<EmotionResult> = listOf(
             EmotionResult(Emotion.ANGER, 20.00f, true),
             EmotionResult(Emotion.DISGUST, 20.00f, false),
-            EmotionResult(Emotion.FEAT, 20.00f, false),
+            EmotionResult(Emotion.FEAR, 20.00f, false),
             EmotionResult(Emotion.JOY, 20.00f, false),
             EmotionResult(Emotion.NEUTRAL, 20.00f, false),
             EmotionResult(Emotion.SADNESS, 20.00f, false),
             EmotionResult(Emotion.SURPRISE, 20.00f, false),
-            )
+        )
     }
 }
