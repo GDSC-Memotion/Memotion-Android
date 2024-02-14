2490e2dd3240f0a01e1475b0d3a09df56251ce62
parkjiwon <ji11won@naver.com>
2024-02-14 15:19:00 +0900

    Fix : 오류 수정
    Hilt 적용 후 Java8을 사용하기 위해서 build.gradle의 java 버전 변경 후 일어난 오류를 위해 수정된 코드

--- app/src/main/java/konkuk/gdsc/memotion/ui/diary/detail/DiaryDetailActivity.kt
+++ app/src/main/java/konkuk/gdsc/memotion/ui/diary/detail/DiaryDetailActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.skydoves.balloon.ArrowOrientation
+import com.skydoves.balloon.Balloon
 import com.skydoves.balloon.BalloonAnimation
 import com.skydoves.balloon.createBalloon
 import konkuk.gdsc.memotion.MainActivity
@@ -21,31 +22,15 @@ class DiaryDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDiaryDetailBinding
     private val data = DiaryDetail.sample
-    private val balloon by lazy {
-        createBalloon(this@DiaryDetailActivity) {
-            setLayout(R.layout.popup_diary_detail)
-            setArrowSize(0)
-            setArrowOrientation(ArrowOrientation.TOP)
-            setCornerRadius(4f)
-            setMarginRight(20)
-            setMarginTop(8)
-            setBackgroundColor(
-                ContextCompat.getColor(
-                    this@DiaryDetailActivity,
-                    R.color.white
-                )
-            )
-            setBalloonAnimation(BalloonAnimation.ELASTIC)
-            setLifecycleOwner(this@DiaryDetailActivity)
-            build()
-        }
-    }
+    private lateinit var balloon: Balloon
     private var emotionState = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDiaryDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        balloon = setBalloon()
 
         setDetailVisibility()
         val titleEmotion =
@@ -121,15 +106,34 @@ class DiaryDetailActivity : AppCompatActivity() {
 
     private fun setDetailVisibility() {
         if (emotionState) {
-            binding.tvDiaryDetailEmotionViewmore.text = this@DiaryDetailActivity.getString(R.string.view_less)
+            binding.tvDiaryDetailEmotionViewmore.text =
+                this@DiaryDetailActivity.getString(R.string.view_less)
             binding.llcDiaryDetailEmotionDetail.visibility = View.VISIBLE
             binding.tvHiddenEmotion.visibility = View.VISIBLE
             binding.tvDiaryDetailEmotion.visibility = View.GONE
         } else {
-            binding.tvDiaryDetailEmotionViewmore.text = this@DiaryDetailActivity.getString(R.string.view_more)
+            binding.tvDiaryDetailEmotionViewmore.text =
+                this@DiaryDetailActivity.getString(R.string.view_more)
             binding.llcDiaryDetailEmotionDetail.visibility = View.GONE
             binding.tvHiddenEmotion.visibility = View.GONE
             binding.tvDiaryDetailEmotion.visibility = View.VISIBLE
         }
     }
+
+    private fun setBalloon(): Balloon = Balloon.Builder(this)
+        .setLayout(R.layout.popup_diary_detail)
+        .setArrowSize(0)
+        .setArrowOrientation(ArrowOrientation.TOP)
+        .setCornerRadius(4f)
+        .setMarginRight(20)
+        .setMarginTop(8)
+        .setBackgroundColor(
+            ContextCompat.getColor(
+                this@DiaryDetailActivity,
+                R.color.white
+            )
+        )
+        .setBalloonAnimation(BalloonAnimation.ELASTIC)
+        .setLifecycleOwner(this@DiaryDetailActivity)
+        .build()
 }
