8f491c647b1ea9e34a91d0de300595a6e82288a9
parkjiwon <ji11won@naver.com>
2024-02-20 02:06:56 +0900

    Feat : ContentUriRequestBody 추가
    - MultiparBody.Part 생성기

---
+++ app/src/main/java/konkuk/gdsc/memotion/util/image/ContentUriRequestBody.kt
@@ -0,0 +1,56 @@
+package konkuk.gdsc.memotion.util.image
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okio.BufferedSink
+import java.io.ByteArrayOutputStream
+
+class ContentUriRequestBody(
+    context: Context,
+    private val uri: Uri?,
+) : RequestBody() {
+    private val contentResolver = context.contentResolver
+
+    private var fileName = ""
+    private var size = -1L
+    private var compressedImage: ByteArray? = null
+
+    init {
+        if (uri != null) {
+            contentResolver.query(
+                uri,
+                arrayOf(MediaStore.Images.Media.SIZE, MediaStore.Images.Media.DISPLAY_NAME),
+                null,
+                null,
+                null,
+            )?.use { cursor ->
+                if (cursor.moveToFirst()) {
+                    size =
+                        cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE))
+                    fileName =
+                        cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME))
+                }
+            }
+        }
+    }
+
+    private fun getFileName() = fileName
+
+    override fun contentLength(): Long = size
+
+    override fun contentType(): MediaType? =
+        uri?.let { contentResolver.getType(it)?.toMediaTypeOrNull() }
+
+    override fun writeTo(sink: BufferedSink) {
+        compressedImage?.let(sink::write)
+    }
+
+    fun toFormData(name: String) = MultipartBody.Part.createFormData(name, getFileName(), this)
+}
