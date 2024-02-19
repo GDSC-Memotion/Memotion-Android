a10a1461e8a5d629eecab2343227f05b30cadf4d
parkjiwon <ji11won@naver.com>
2024-02-20 02:03:20 +0900

    Fix : ImageAdapter 오류 수정
    - 중간의 사진이 없어지지 않는 오류 수정
    - ImageType enum class 적용

--- app/src/main/java/konkuk/gdsc/memotion/ui/diary/create/ImageAdapter.kt
+++ app/src/main/java/konkuk/gdsc/memotion/ui/diary/create/ImageAdapter.kt
@@ -13,12 +13,14 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import konkuk.gdsc.memotion.databinding.ItemAddPhotoBinding
 import konkuk.gdsc.memotion.databinding.ItemPhotoBinding
+import konkuk.gdsc.memotion.domain.entity.image.Image
+import konkuk.gdsc.memotion.domain.entity.image.ImageType
 import konkuk.gdsc.memotion.util.TAG
 import konkuk.gdsc.memotion.util.view.setOnSingleClickListener
 
 class ImageAdapter(
     private val context: Context,
-    private val data: MutableList<String>,
+    val data: MutableList<Image>,
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val pickMultipleMedia = (context as AppCompatActivity).registerForActivityResult(
@@ -38,16 +40,16 @@ class ImageAdapter(
         val binding: ItemPhotoBinding,
         private val context: Context,
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: String) {
+        fun bind(item: Image) {
             Glide.with(context)
-                .load(item)
+                .load(item.url)
                 .centerCrop()
                 .into(binding.ivItemPhoto)
         }
     }
 
     override fun getItemViewType(position: Int): Int {
-        return if (data[position].isNullOrBlank()) 0 else 1
+        return if (position == 0) 0 else 1
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -67,7 +69,7 @@ class ImageAdapter(
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (data[position].isNullOrBlank()) {
+        if (position == 0) {
             val emptyViewHolder = holder as EmptyViewHolder
             emptyViewHolder.binding.llcAddPhoto.setOnSingleClickListener {
                 pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
@@ -75,16 +77,16 @@ class ImageAdapter(
 
         } else {
             val imageViewHolder = holder as ImageViewHolder
-            imageViewHolder.bind(data[position])
+            imageViewHolder.bind(data[position - 1])
             imageViewHolder.binding.ivItemPhotoClose.setOnClickListener {
-                data.removeAt(position)
+                data.removeAt(position - 1)
 //                notifyItemRemoved(position) // -> 이상한 오류가 존재 중간에 것들을 삭제한 경우 position이 제대로 바뀌지 않는 오류 발견
                 notifyDataSetChanged()
             }
         }
     }
 
-    override fun getItemCount() = data.size
+    override fun getItemCount() = data.size + 1
 
     private fun updateData(urls: List<Uri>) {   // 새로운 조건이 필요함
         if (validateUrls(urls)) {
@@ -95,15 +97,19 @@ class ImageAdapter(
     }
 
     private fun addData(urls: List<Uri>) {
-        val dataStart = data.size
         for (url in urls) {
-            data.add(url.toString())
+            data.add(
+                Image(
+                    url = url.toString(),
+                    type = ImageType.NEW,
+                )
+            )
         }
-        notifyItemRangeInserted(dataStart, urls.size)
+        notifyItemInserted(data.size + 1)
     }
 
     private fun errorData() {
-        Toast.makeText(context, "사진의 갯수가 잘 못되었습니다.", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "The number of photos is incorrect.", Toast.LENGTH_SHORT).show()
     }
 
     private fun validateUrls(urls: List<Uri>): Boolean {
@@ -115,7 +121,13 @@ class ImageAdapter(
     }
 
     fun setData(newData: MutableList<String>) {
-        data.addAll(newData)
+        val new = newData.map {
+            Image(
+                url = it,
+                type = ImageType.EXISTING
+            )
+        }
+        data.addAll(new)
     }
 
     companion object {
