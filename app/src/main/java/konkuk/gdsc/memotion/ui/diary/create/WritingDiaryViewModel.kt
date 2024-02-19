8b515bafcacae86b93e842951df48453f8d41983
parkjiwon <ji11won@naver.com>
2024-02-20 02:05:21 +0900

    Feat : WritingDiaryViewModel 기능 추가
    - newImage : 새로운 사진 저장
    - emotionResult : 일기 감정 분석 결과 적용
    - postDiary, editDiary, setVersion, setDiaryData, setEmotionResult, updateImages 등 함수 추가

--- app/src/main/java/konkuk/gdsc/memotion/ui/diary/create/WritingDiaryViewModel.kt
+++ app/src/main/java/konkuk/gdsc/memotion/ui/diary/create/WritingDiaryViewModel.kt
@@ -1,13 +1,20 @@
 package konkuk.gdsc.memotion.ui.diary.create
 
 import android.icu.util.Calendar
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import konkuk.gdsc.memotion.domain.entity.diary.DiaryWriting
-import konkuk.gdsc.memotion.domain.entity.user.User
+import konkuk.gdsc.memotion.domain.entity.emotion.Emotion
 import konkuk.gdsc.memotion.domain.repository.DiaryRepository
+import konkuk.gdsc.memotion.util.NETWORK
+import konkuk.gdsc.memotion.util.calendarToString
+import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,4 +26,68 @@ class WritingDiaryViewModel @Inject constructor(
 
     private val _diaryData = MutableLiveData<DiaryWriting>()
     val diaryData: LiveData<DiaryWriting> = _diaryData
+
+    private val _newImages = MutableLiveData<MutableList<MultipartBody.Part>>()
+    val newImages: LiveData<MutableList<MultipartBody.Part>> = _newImages
+
+    private val _emotionResult = MutableLiveData<Emotion>()
+    val emotionResult: LiveData<Emotion> = _emotionResult
+
+    init {
+        _version.value = DiaryVersion.WRITING
+        _diaryData.value = DiaryWriting(
+            date = calendarToString(Calendar.getInstance()),
+            content = "",
+            imageUrls = listOf()
+        )
+        _newImages.value = mutableListOf()
+    }
+
+    fun postDiary() {
+        viewModelScope.launch {
+            val diary = diaryData.value?.asDiaryWithoutImages()
+                ?: DiaryWriting.sample.asDiaryWithoutImages()
+            diaryRepository.postDiary(diary, newImages.value ?: listOf())
+                .onSuccess {
+                    Log.d(NETWORK, "WritingDiaryViewModel - postDiary() called\nsuccess")
+                    _emotionResult.value = when (it.result.emotion.uppercase(Locale.getDefault())) {
+                        "DISGUST" -> Emotion.DISGUST
+                        "FEAR" -> Emotion.FEAR
+                        "JOY" -> Emotion.JOY
+                        "NEUTRAL" -> Emotion.NEUTRAL
+                        "SADNESS" -> Emotion.SADNESS
+                        "SURPRISE" -> Emotion.SURPRISE
+                        else -> Emotion.ANGER
+                    }
+                }
+                .onFailure {
+                    Log.d(NETWORK, "WritingDiaryViewModel - postDiary() called\nfailed\nbecause:$it")
+                }
+        }
+    }
+
+    fun editDiary() {
+        viewModelScope.launch {
+//            diaryRepository.putDiary()
+        }
+    }
+
+    fun setVersion(version: Int) {
+        _version.value = when (version) {
+            1 -> DiaryVersion.EDITING
+            else -> DiaryVersion.WRITING
+        }
+    }
+
+    fun setDiaryData(data: DiaryWriting) {
+        _diaryData.value = data
+    }
+
+    fun setEmotionResult(emotion: Emotion) {
+        _emotionResult.value = emotion
+    }
+
+    fun updateImages(images: List<MultipartBody.Part>) {
+        _newImages.value = images.toMutableList()
+    }
 }
