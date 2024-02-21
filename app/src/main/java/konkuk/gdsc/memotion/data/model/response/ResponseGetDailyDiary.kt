649aa7579408b806d543e8e8135c6f8b2d78180f
parkjiwon <ji11won@naver.com>
2024-02-21 16:53:53 +0900

    Feat : DailyDiary parameter 변경 및 변경 추가

--- app/src/main/java/konkuk/gdsc/memotion/data/model/response/ResponseGetDailyDiary.kt
+++ app/src/main/java/konkuk/gdsc/memotion/data/model/response/ResponseGetDailyDiary.kt
@@ -4,6 +4,7 @@ import konkuk.gdsc.memotion.domain.entity.diary.DiarySimple
 import konkuk.gdsc.memotion.domain.entity.emotion.Emotion
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import java.util.Locale
 
 @Serializable
 data class ResponseGetDailyDiary(
@@ -19,15 +20,25 @@ data class ResponseGetDailyDiary(
         @SerialName("memberId")
         val memberId: Long,
         @SerialName("mood")
-        val mood: Int,
+        val mood: String,
         @SerialName("imageUris")
         val imageUris: List<String>,
     ) {
         fun asDiarySimple(): DiarySimple {
+            val emotion = when (mood) {
+                Emotion.ANGER.toString().lowercase(Locale.getDefault()) -> Emotion.ANGER
+                Emotion.DISGUST.toString().lowercase(Locale.getDefault()) -> Emotion.DISGUST
+                Emotion.FEAR.toString().lowercase(Locale.getDefault()) -> Emotion.FEAR
+                Emotion.JOY.toString().lowercase(Locale.getDefault()) -> Emotion.JOY
+                Emotion.NEUTRAL.toString().lowercase(Locale.getDefault()) -> Emotion.NEUTRAL
+                Emotion.SADNESS.toString().lowercase(Locale.getDefault()) -> Emotion.SADNESS
+                Emotion.SURPRISE.toString().lowercase(Locale.getDefault()) -> Emotion.SURPRISE
+                else -> Emotion.ANGER
+            }
             return DiarySimple(
                 diaryId = id,
                 date = description,
-                emotion = Emotion.values()[mood],
+                emotion = emotion,
                 content = description,
                 imageUrl = imageUris
             )
