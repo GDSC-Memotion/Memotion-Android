2cf81eef1051e5ec4865fe86342efa4132e05413
parkjiwon <ji11won@naver.com>
2024-02-22 11:08:09 +0900

    Feat : 날짜와 사진 추가

--- app/src/main/java/konkuk/gdsc/memotion/ui/diary/detail/DiaryDetailActivity.kt
+++ app/src/main/java/konkuk/gdsc/memotion/ui/diary/detail/DiaryDetailActivity.kt
@@ -5,12 +5,14 @@ import android.icu.util.Calendar
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.skydoves.balloon.ArrowOrientation
 import com.skydoves.balloon.Balloon
 import com.skydoves.balloon.BalloonAnimation
@@ -21,7 +23,9 @@ import konkuk.gdsc.memotion.domain.entity.diary.DiaryDetail
 import konkuk.gdsc.memotion.databinding.ActivityDiaryDetailBinding
 import konkuk.gdsc.memotion.ui.diary.DiaryAdapter
 import konkuk.gdsc.memotion.ui.diary.create.WritingDiaryActivity
+import konkuk.gdsc.memotion.util.TAG
 import konkuk.gdsc.memotion.util.calendarToString
+import konkuk.gdsc.memotion.util.dpToPx
 
 @AndroidEntryPoint
 class DiaryDetailActivity : AppCompatActivity() {
@@ -42,8 +46,15 @@ class DiaryDetailActivity : AppCompatActivity() {
             val titleEmotion =
                 requireNotNull(it.emotions.find { it.isTitle }) { "emotion data is null" }
             binding.apply {
-//                tvDiaryDetailDate.text = it.date
-                tvDiaryDetailDate.text = calendarToString(Calendar.getInstance())
+                tvDiaryDetailDate.text = it.date
+                if (it.imageUrls.isNotEmpty()) {
+                    Glide.with(this@DiaryDetailActivity)
+                        .load(it.imageUrls[0])
+                        .centerCrop()
+                        .into(ivDiaryDetailImage)
+                    ivDiaryDetailImage.visibility = View.VISIBLE
+                    ivDiaryDetailImage.layoutParams.height = resources.displayMetrics.widthPixels
+                }
                 tvDiaryDetailContent.text = it.content
 
                 tvDiaryDetailEmotion.text = titleEmotion.emotion.toString()
@@ -122,13 +133,13 @@ class DiaryDetailActivity : AppCompatActivity() {
                 this@DiaryDetailActivity.getString(R.string.view_less)
             binding.llcDiaryDetailEmotionDetail.visibility = View.VISIBLE
             binding.tvHiddenEmotion.visibility = View.VISIBLE
-            binding.tvDiaryDetailEmotion.visibility = View.GONE
+            binding.llDiaryDetailEmotion.visibility = View.GONE
         } else {
             binding.tvDiaryDetailEmotionViewmore.text =
                 this@DiaryDetailActivity.getString(R.string.view_more)
             binding.llcDiaryDetailEmotionDetail.visibility = View.GONE
             binding.tvHiddenEmotion.visibility = View.GONE
-            binding.tvDiaryDetailEmotion.visibility = View.VISIBLE
+            binding.llDiaryDetailEmotion.visibility = View.VISIBLE
         }
     }
 
